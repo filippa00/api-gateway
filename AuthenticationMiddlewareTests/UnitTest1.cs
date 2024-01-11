6921f07bbad44f7e0318eb343abe47ac5fed2005
filippa00 <filippa00@users.noreply.github.com>
2024-01-11 09:51:29 +0100

    more changes

---
+++ AuthenticationMiddlewareTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AuthenticationMiddlewareTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
